bba737a8d04567d52c91b8bb1b5493718f6d25be
Ren <Ren>
2017-01-30 21:44:27 +0900

    update README

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,17 @@
-<?php
-
+﻿<?php
 // 3で割り切れる時 Fizz
 // 5で割り切れる時 Buzz
 // 15 で割り切れる時 FizzBuzz
-
 for ($i=1; $i <=100 ; $i++) {
     if ($i % 15 === 0) {
         $result = "FizzBuzz";
+    }elseif($i % 3 === 0){
+	$result = "Fizz";
+    }elseif($i % 5 === 0){
+	$result = "Buzz";
+    }else{
+	$result = $i;
     }
     echo $result;
+    echo ("\n");
 }
